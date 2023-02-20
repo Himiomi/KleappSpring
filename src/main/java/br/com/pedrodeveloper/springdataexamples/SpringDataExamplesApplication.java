3a64182e4f7e95fa080d985500716917be1a2f63
Florian Morin <fmorin@et.esiea.fr>
2023-02-20 18:29:48 +0100

    create project

--- src/main/java/br/com/pedrodeveloper/springdataexamples/SpringDataExamplesApplication.java
+++ src/main/java/br/com/pedrodeveloper/springdataexamples/SpringDataExamplesApplication.java
@@ -1,11 +1,24 @@
 package br.com.pedrodeveloper.springdataexamples;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages={"br.com.pedrodeveloper.springdataexamples.*"})
 public class SpringDataExamplesApplication {
 
+	@Bean
+	public OpenAPI springShopOpenAPI(@Value("${application-version}") String appVersion,@Value("${application-description}") String appDescription) {
+		return new OpenAPI()
+				.info(new Info().title("Api backend du projet Kleapp :)")
+						.description(appDescription)
+						.version(appVersion)
+						.license(new License().name("MIT")));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataExamplesApplication.class, args);
 	}
