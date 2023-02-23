a10da409d6642cf2f140132776bcdbfaf12a8a56
Florian Morin <fmorin@et.esiea.fr>
2023-02-23 13:34:09 +0100

    change type of database and remote

--- src/main/java/br/com/pedrodeveloper/springdataexamples/repository/VendaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pedrodeveloper.springdataexamples.repository;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import br.com.pedrodeveloper.springdataexamples.entities.Venda;
-
-public interface VendaRepository extends PagingAndSortingRepository<Venda, Integer>{
-
-}
